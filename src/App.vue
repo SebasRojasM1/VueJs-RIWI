51bf9241f1f21b0487513ae0f3513ed46c9d509b
Sebastian <sebasrojasm1@gmail.com>
2024-08-14 16:49:07 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" src="./assets/styles/main.scss"></style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './assets/styles/main.scss'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+
+createApp(App).mount('#app')
