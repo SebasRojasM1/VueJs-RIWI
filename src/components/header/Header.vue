46d75ba1954db81c26cd8bc6e86c20dd806c2fd6
Sebastian <sebasrojasm1@gmail.com>
2024-08-14 17:02:26 -0500

    The Banner of the Header was implemente, with dinamic info and the connect/connected button

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,13 +1,14 @@
 <template>
   <header class="header">
     <SearchBar :headerInfo="headerInfo" />
-
+    <Banner :headerInfo="headerInfo" :isConnected="isConnected" @toggleConnection="toggleConnection" />
   </header>
 </template>
 
 <script setup lang="ts">
 import { ref, defineProps, defineEmits } from 'vue';
 import SearchBar from './components/SearchBar.vue';
+import Banner from './components/Banner.vue';
 import avatar from "../../assets/images/user.jpg"
 import banner from "../../assets/images/banner.jpg"
 
@@ -20,6 +21,26 @@ const headerInfo = ref({
   bannerImage: banner,
 });
 
+
+// Estado del botón
+const isConnected = ref(true);
+
+// Alternar estado del botón
+function toggleConnection() {
+  isConnected.value = !isConnected.value;
+}
+
+/*Logica para traer el cambio de pagina y aplicarle luego los estilos*/
+// Recibir la página activa como prop
+const props = defineProps({
+  activePage: String,
+});
+
+const emit = defineEmits(['changePage']);
+
+function changePage(page: string) {
+  emit('changePage', page);
+}
 </script>
 
 <style lang="scss" scoped>
