d973013607d4da6d6cbfaa078a4e70ceb8fd4a5f
Sebastian <sebasrojasm1@gmail.com>
2024-08-14 16:58:23 -0500

    The SearchBar of the Header was implemented

---
+++ src/components/header/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <SearchBar :headerInfo="headerInfo" />
+
+  </header>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, defineEmits } from 'vue';
+import SearchBar from './components/SearchBar.vue';
+import avatar from "../../assets/images/user.jpg"
+import banner from "../../assets/images/banner.jpg"
+
+const headerInfo = ref({
+  name: 'John Doe',
+  role: 'Coder',
+  location: 'Medellin',
+  joinDate: 'April 2021',
+  avatar: avatar,
+  bannerImage: banner,
+});
+
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/variables';
+@import '../../assets/styles/mixins';
+
+.header {
+  min-width: 100%;
+}
+</style>
---
+++ src/components/header/components/SearchBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="navbar">
+    <div class="search-bar">
+      <input type="text" placeholder="Search ⌘K" />
+    </div>
+
+    <div class="user-actions">
+      <button class="action-button"><i class="fa-solid fa-arrow-down-a-z"></i></button>
+      <button class="action-button"><i class="fa-regular fa-sun"></i></button>
+      <button class="action-button"><i class="fa-solid fa-border-all"></i></button>
+      <button class="action-button"><i class="fa-regular fa-bell"></i></button>
+      <img class="user-avatar" :src="headerInfo.avatar" alt="User Avatar" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  headerInfo: Object,
+});
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/mixins';
+  @import '../../../assets/styles/variables';
+
+.navbar {
+  @include flex-center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  background-color: $card-background-color;
+  box-shadow: $card-box-shadow;
+  margin: 0 0 35px;
+  border-radius: 8px;
+
+  .search-bar {
+    margin-left: 18px;
+    input {
+      padding: 10px 8px;
+      border-radius: 8px;
+      border: none;
+      width: 690%;
+    }
+  }
+
+  .user-actions {
+    display: flex;
+    align-items: center;
+
+    .action-button {
+      background: none;
+      border: none;
+      margin-left: 8px;
+      cursor: pointer;
+      font-size: 16px;
+    }
+
+    .user-avatar {
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+      margin-left: 8px;
+      object-fit: cover;
+    }
+  }
+}
+</style>
