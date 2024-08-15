2e220c62b71bb08e9016e773751f9cc9d6a53a96
Sebastian <sebasrojasm1@gmail.com>
2024-08-14 21:28:37 -0500

    In the script, was added the images and the object with the dinamic info of Teams Component

--- src/components/profile/components/Teams.vue
+++ src/components/profile/components/Teams.vue
@@ -27,3 +27,52 @@
       </ul>
     </section>
   </template>
+
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  
+  import reactLogo from '../../../assets/images/logos/react.png'
+  import figmaLogo from '../../../assets/images/logos/figma.png'
+  import twitterLogo from '../../../assets/images/logos/twitter.png'
+  import vuejsLogo from '../../../assets/images/logos/vuejs.png'
+  import worldLogo from '../../../assets/images/logos/worls.png'
+  
+ const teams = ref([
+  {
+    id: 1,
+    name: "React Developers",
+    members: 72,
+    tag: "Developer",
+    icon: reactLogo
+  },
+  {
+    id: 2,
+    name: "Support Team",
+    members: 122,
+    tag: "Support",
+    icon: worldLogo
+  },
+  {
+    id: 3,
+    name: "UI Designer",
+    members: 7,
+    tag: "Designer",
+    icon: figmaLogo
+  },
+  {
+    id: 4,
+    name: "Vue.Js Developers",
+    members: 289,
+    tag: "Developer",
+    icon: vuejsLogo
+  },
+  {
+    id: 5,
+    name: "Digital Marketing",
+    members: 24,
+    tag: "Marketing",
+    icon: twitterLogo
+  }
+])
+</script>
