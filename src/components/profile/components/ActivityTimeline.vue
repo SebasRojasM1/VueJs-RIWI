704d9475671bbf3c0af17a06d86b5720113be726
Sebastian <sebasrojasm1@gmail.com>
2024-08-14 21:14:28 -0500

    The styles of the component ActivityTimeline were made according to the mockup, with variables in SASS and Mixins

--- src/components/profile/components/ActivityTimeline.vue
+++ src/components/profile/components/ActivityTimeline.vue
@@ -42,3 +42,139 @@
       </ul>
     </section>
   </template>
+
+
+<script setup lang="ts">
+
+</script>
+
+
+<style lang="scss" scoped>
+@import '../../../assets/styles/mixins';
+@import '../../../assets/styles/variables';
+
+.activity-timeline {
+grid-column: span 4 / span 4; // Ocupa las columnas 2 a 5
+grid-row: span 1 / span 1; // Ocupa solo la fila necesaria
+background-color: $card-background-color;
+border-radius: $card-border-radius;
+box-shadow: $card-box-shadow;
+padding: 20px;
+color: $color-paragraph-text;
+
+h3 {
+  color: $color-title-text;
+  font-weight: bold;
+  margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+
+  .timeline-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-bottom: 20px;
+    position: relative;
+
+    .item-content {
+      max-width: 80%; // Para ajustar el espacio del contenido
+      font-size: 0.9rem;
+      margin-bottom: 5px;
+
+      strong {
+        color: $color-title-text;
+      }
+
+      .document-link {
+        display: flex;
+        align-items: center;
+        color: $color-button-principal;
+        font-size: 0.9rem;
+        margin-top: 5px;
+        background-color: #eeeeef;
+        padding: 10px;
+        border-radius: 5px;
+        color: #a8a6ad;
+        text-decoration: none;
+        width: 110px;
+        object-fit: cover;
+
+        i {
+          margin-right: 8px;
+          color: red;
+        }
+      }
+
+      .user-info {
+        display: flex;
+        align-items: center;
+        margin-top: 10px;
+
+        .user-image {
+          width: 32px;
+          height: 32px;
+          border-radius: 50%;
+          margin-right: 10px;
+          object-fit: cover;
+        }
+
+        .user-details {
+          font-size: 0.85rem;
+          color: $color-paragraph-text;
+        }
+      }
+
+      .team-members {
+        display: flex;
+        align-items: center;
+        margin-top: 10px;
+
+        .team-image {
+          width: 32px;
+          height: 32px;
+          border-radius: 50%;
+          margin-right: 5px;
+          border: 2px solid white;
+
+          &:nth-child(2){
+            position: relative;
+            right: 17px;
+          }
+
+          &:nth-child(3){
+            position: relative;
+            right: 32px;
+          }
+        }
+
+        .more-members {
+          @include flex-center;
+          padding: 8px;
+          border-radius: 50%;
+          background-color: $color-button-principal;
+          color: $color-button-secundary;
+          font-size: 0.85rem;
+          font-weight: bold;
+          position: relative;
+          right: 49px;
+          border: 2px solid white;
+        }
+      }
+    }
+
+    .item-time {
+      font-size: 0.8rem;
+      color: $color-paragraph-text;
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  }
+}
+}
+</style>
