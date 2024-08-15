e4d9e9658de00cba6220e66493187d664700dec0
Sebastian <sebasrojasm1@gmail.com>
2024-08-14 21:23:39 -0500

    The styles of the component were made according to the mockup, with variables in SASS

--- src/components/profile/components/Connections.vue
+++ src/components/profile/components/Connections.vue
@@ -79,3 +79,114 @@
   }
 ])
 </script>
+
+
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/mixins';
+  @import '../../../assets/styles/variables';
+  
+  .connections {
+    grid-column: span 2 / span 2;
+    grid-row: span 1 / span 1; // Ocupa solo la fila 3
+    grid-column-start: 2; // Comienza en la columna 2
+    grid-row-start: 2; // Comienza en la fila 3
+    background-color: $card-background-color;
+    border-radius: $card-border-radius;
+    box-shadow: $card-box-shadow;
+    padding: 20px 20px;
+    color: $color-paragraph-text;
+
+    .head-section{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      h3 {
+        color: $color-title-text;
+        font-weight: bold;
+        margin-left: 10px;
+      } 
+
+      p{
+        color: $color-title-text;
+        font-weight: bold;
+        font-size: 21px;
+        margin-right: 38px;      
+      }
+    }
+    
+
+    ul {
+      list-style: none;
+      padding: 0;
+
+      .connection-item {
+        margin: 20px 10px;
+
+        .item-info {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+
+          img{
+            border-radius: 50%;
+          }
+
+          .user-container{
+            @include flex-center;
+
+            .img-user img{
+              width: 40px;
+              margin-right: 10px
+            }
+
+            .info-user{
+              margin: 0;
+
+              .item-name {
+                font-weight: bold;
+                font-size: 17px;
+                margin: 0;
+              }
+
+              .item-number-users {
+                font-size: 15px;
+                margin: 3px 0 0;
+                color: $color-paragraph-text;
+              }
+            }
+          }
+
+          .connected-button button{
+            background: none;
+            margin: 0;
+            border: none;
+          }
+
+          .connected-button i{
+            background-color: $color-button-principal;
+            color: white;
+            border: none;
+            align-items: center;
+            text-align: center;
+            border-radius: 8px;
+            justify-content: center;
+            padding: 12px;
+            cursor: pointer;
+          }
+
+        }
+      }
+    }
+
+    a{
+        @include flex-center;
+        text-decoration: none;
+        color: $purple-font-color;
+        font-weight: 600;
+        margin-top: 10px;
+        font-size: 17px;
+      }
+  }
+</style>
